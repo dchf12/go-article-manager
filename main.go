071cf067f9dfb84228a87b1d11036f87bdf330d7
dchf <dchf.jp@gmail.com>
2022-06-19 21:56:54 +0900

    Refactor index handler

--- main.go
+++ main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(
-			http.StatusOK,
-			"index.html",
-			gin.H{
-				"title": "Home Page",
-			},
-		)
-	})
+	r.GET("/", showIndexPage)
 	r.Run()
 }
