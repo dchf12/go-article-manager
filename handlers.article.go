f61005902fe22b32c7afe69edbdda39a77010163
dchf <dchf.jp@gmail.com>
2022-06-26 11:29:10 +0900

    Update route handler

--- handlers.article.go
+++ handlers.article.go
@@ -10,15 +10,10 @@ import (
 func showIndexPage(c *gin.Context) {
 	articles := getAllArticles()
 
-	// Call the HTML method of the Context to render a template
-	c.HTML(
-		http.StatusOK,
-		"index.html",
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		},
-	)
+	// Call the render function with the name of the template to render
+	render(c, gin.H{
+		"title":   "Home Page",
+		"payload": articles}, "index.html")
 }
 
 func getArticle(c *gin.Context) {
@@ -31,15 +26,10 @@ func getArticle(c *gin.Context) {
 		c.AbortWithError(http.StatusNotFound, err)
 	}
 
-	// Call the HTML method of the Context to render a template
-	c.HTML(
-		http.StatusOK,
-		"article.html",
-		gin.H{
-			"title":   article.Title,
-			"payload": article,
-		},
-	)
+	// Call the render function with the name of the template to render
+	render(c, gin.H{
+		"title":   article.Title,
+		"payload": article}, "article.html")
 }
 
 // Render one of HTML, JSON or CSV based on the 'Accept' header of the request
